ba1401b05a770e0bccddbdbc1dc8fc575b741e4a
10-24 <1750011571@qq.com>
2022-12-12 11:15:00 +0800

    Create Paste.py

---
+++ Paste.py
@@ -0,0 +1,23 @@
+import pyperclip
+from pynput.keyboard import Controller,Listener,Key
+import time
+
+def paste():
+    time.sleep(1)
+    #按下后间隔1s
+    i=pyperclip.paste()
+    keyboard = Controller()
+    keyboard.type(i)
+    print("粘贴成功"+"\n"+i+"\n")
+
+def on_release(key):
+    if key == Key.esc:
+        # 停止监听
+        return False
+    if key == Key.f2:
+        paste()
+        # 这里是按f2调用粘贴函数
+
+with Listener(
+        on_release=on_release) as listener:
+    listener.join()
